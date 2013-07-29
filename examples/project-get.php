6977fdf2518831a37fa6df2aeded1db277232e8e
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2013-07-29 21:38:58 +0700

    Implement new Client object in example

--- examples/project-get.php
+++ examples/project-get.php
@@ -1,31 +1,7 @@
 <?php
-
-/*
- * Copyright 2011, Cloudwords, Inc.
- *
- * Licensed under the API LICENSE AGREEMENT, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.cloudwords.com/developers/license-1.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-require_once "bootstrap.php";
-
-// create an instance of the api client
-const BASE_API_URL = 'https://api-stage.cloudwords.com';
-const API_VERSION = '1';
-const AUTHENTICATION_TOKEN = 'UserToken <ENTER_AUTHORIZATION_TOKEN_HERE>';
-
-$client = new CloudwordsClient(BASE_API_URL, API_VERSION, AUTHENTICATION_TOKEN);
-
-// get detail project
-$project = $client->get_project(10173);
-print_r($project);
-
+require_once '../src/Client.php';
+$baseApiUrl = 'https://api-stage.cloudwords.com';
+$apiVersion = '1';
+$authToken  = 'UserToken <ENTER_AUTHORIZATION_TOKEN_HERE>';
+$client = new \Cloudwords\Client($baseApiUrl, $apiVersion, $authToken);
+print_r($client->getProject(10173));
